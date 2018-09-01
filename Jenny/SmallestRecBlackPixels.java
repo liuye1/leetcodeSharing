7293824503038e0a0adcc249b497c64eaac0804f
szhang89 <szhang9300@gmail.com>
2018-09-01 11:55:25 -0400

    leetcode302

--- Jenny/SmallestRecBlackPixels.java
+++ Jenny/SmallestRecBlackPixels.java
@@ -1,3 +1,5 @@
+//LeetCode302
+//https://leetcode.com/problems/smallest-rectangle-enclosing-black-pixels/description/
 // An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel.
 // The black pixels are connected, i.e., there is only one black region. Pixels are connected horizontally and vertically.
 // Given the location (x, y) of one of the black pixels, 
